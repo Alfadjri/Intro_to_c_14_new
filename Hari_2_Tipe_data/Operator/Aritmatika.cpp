b4df16ca110c76cc42a940703382fd9cb25a28e7
Alfadjri Dwi Fadhilah <alfadjri28@gmail.com>
2024-11-22 08:13:04 +0700

    Hari ke 3
    Operation Aritmatika
    Operation Comperation
    Operation Loops
    Operation Selection
    LocalStorage Tree_Map

--- Hari_2_Tipe_data/Operator/Aritmatika.cpp
+++ Hari_2_Tipe_data/Operator/Aritmatika.cpp
@@ -7,7 +7,6 @@ int main(){
     // int hasil;
     int x = 3 , y = 2 , hasil;
     float hasil_float;
-
     // penjumlahan
     hasil = x + y;
     cout << "Hasil tambah dari " << x << " + " << y << " = " << hasil << endl;
@@ -25,5 +24,15 @@ int main(){
     // modulus (sisi dari pembagian)
     hasil = x % y;
     cout << "Hasil tambah dari " << x << " % " << y << " = " << hasil << endl;
+
+    // incremnet
+    int z = 10000;
+    z = 1 ;
+    z = z + 1;
+    // tambahan nilai z dengan 1 ;
+    // z = z + 1;
+    z++; // 10002;
+    z--;
+
     return 0;
 }
